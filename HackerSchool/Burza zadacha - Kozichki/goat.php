72164682d9c9757f33087a91fa03e759be004194
velioo <velioocs@mail.bg>
2017-09-13 11:57:08 +0300

    Done the Fast task - Kozichki

---
+++ HackerSchool/Burza zadacha - Kozichki/goat.php
@@ -0,0 +1,60 @@
+<?php
+
+$first_line = array(15, 3);
+$goat_weights = array(666, 42, 7, 13, 400, 511, 600, 200, 202, 111, 313, 94, 280, 72, 42);
+
+$goats_num = $first_line[0];
+$max_courses = $first_line[1];
+
+$temp_courses = 0;
+
+rsort($goat_weights);
+
+$min_capacity = $goat_weights[0];
+
+$temp_goat_weight = 0;
+
+$goats_moved_count = 0;
+$goats_moved_last_course = 0;
+
+$current_goat_weights = $goat_weights;
+$temp_current_goat_weights = array();
+
+while(true) {
+	
+	$temp_goat_weight = 0;
+	
+	for($i = 0; $i < count($current_goat_weights); $i++) {	//  	
+		
+		if( ($temp_goat_weight + $current_goat_weights[$i]) <= $min_capacity ) { // 
+			
+			$temp_goat_weight+=$current_goat_weights[$i]; // 30, 26, 30
+			$goats_moved_count++; // 1, 2, 3
+			
+		} else {
+			array_push($temp_current_goat_weights, $current_goat_weights[$i]);
+		}
+	}
+	
+	$temp_courses++; // 1, 2
+	
+	$current_goat_weights = $temp_current_goat_weights;
+	$temp_current_goat_weights = array();
+	
+	if( (($temp_courses >= $max_courses) && ($goats_moved_count != count($goat_weights))) ) {
+		$current_goat_weights = $goat_weights;
+		$temp_courses = 0;
+		$goats_moved_count = 0;
+		$min_capacity++;
+	} else {
+		if($goats_moved_count == count($goat_weights)) {
+			break;
+		}
+	}
+
+}
+
+
+echo $min_capacity;
+
+?>
