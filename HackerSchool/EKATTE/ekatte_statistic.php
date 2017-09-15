01508d17ef1419325ba384244929e86d8c14bd0f
velioo <velioocs@mail.bg>
2017-09-15 12:45:03 +0300

    completedwhole ekatte parsing script. Created the ekatte statistics script

--- HackerSchool/EKATTE/ekatte_statistic.php
+++ HackerSchool/EKATTE/ekatte_statistic.php
@@ -7,15 +7,19 @@ include 'user_pass_ekatte.php';
 		die("Connection failed: " . $conn->connect_error);
 	} 	  
 	//$sql = "SELECT COUNT(oblast) as oblasti_count, COUNT(obshtina) as obshtini_count, COUNT(kmetstvo) as selishta_count FROM oblasti, obshtini, selishta;"; 
-	$sql = "SELECT COUNT(oblast) as oblasti_count FROM oblasti"; 
+	$sql = "SELECT COUNT(*) as oblasti_count, (SELECT COUNT(*) FROM obshtini) as obshtini_count, (SELECT COUNT(*) FROM selishta) as selishta_count FROM oblasti"; 
 	if ($result = $conn->query($sql)) {
 		if($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()) {
-				//echo "Objects in Oblasti: " . $row["oblasti_count"]. "<br> Objects in Obshtini: " . $row["obshtini_count"]. "<br> Objects in Selishta: " . $row["selishta_count"]. "<br>";
-				echo "Objects in Oblasti: " . $row["oblasti_count"];
-			}
+			$row = $result->fetch_assoc();
+			$results = "Objects in Oblasti: " . $row["oblasti_count"]. "\nObjects in Obshtini: " . $row["obshtini_count"]. "\nObjects in Selishta: " . $row["selishta_count"]. "\n";
+			$file = fopen("results.txt", "w") or die("Unable to open file!");
+			fwrite($file, $results);
+			fclose($file);
+			echo "Objects in Oblasti: " . $row["oblasti_count"]. "<br> Objects in Obshtini: " . $row["obshtini_count"]. "<br> Objects in Selishta: " . $row["selishta_count"]. "<br>";	
 		}
-	} 
+	} else {
+		echo("Error description: " . mysqli_error($conn));
+	}
 	
 	$conn->close(); 
 
--- HackerSchool/EKATTE/parse_ekatte_files.php
+++ HackerSchool/EKATTE/parse_ekatte_files.php
@@ -7,7 +7,8 @@ include 'user_pass_ekatte.php';
 
 //$fileName = "ekatte_files/Ek_obl.xls";
 //$fileName = "ekatte_files/Ek_obst.xls";
-$fileName = "ekatte_files/Ek_kmet.xls";
+//$fileName = "ekatte_files/Ek_kmet.xls";
+$fileName = "ekatte_files/Ek_atte.xls";
 
 try {
 
@@ -28,26 +29,33 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 } 
 
+mysqli_set_charset($conn,"utf8");
+
 //$stmt = $conn->prepare("INSERT INTO oblasti (oblast, ekatte_num, name, region, document, abc) VALUES (?, ?, ?, ?, ?, ?)"); //oblasti
 //$stmt = $conn->prepare("INSERT INTO obshtini (obshtina, ekatte_num, name, category, document, abc) VALUES (?, ?, ?, ?, ?, ?)"); // obshtini
-$stmt = $conn->prepare("INSERT INTO selishta (kmetstvo, name, center, document) VALUES (?, ?, ?, ?)"); //selishta
+//$stmt = $conn->prepare("INSERT INTO kmetstva (kmetstvo, name, center, document) VALUES (?, ?, ?, ?)"); // kmetstva
+$stmt = $conn->prepare("INSERT INTO selishta (ekatte_num, type, name, oblast_f, obstina_f, kmetstvo_f, category, altitude, document, tsb, abc) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); //selishta
 //$param_types = "sissii"; // oblasti
 //$param_types = "sisiii"; // obshtini
-$param_types = "ssii"; // selishta
+//$param_types = "ssii"; // kmetstva
+$param_types = "ssssssiiisi"; // selishta
 
-for ($row = 2; $row <= $highestRow; $row++){ 
+
+for ($row = 3; $row <= $highestRow; $row++){ // oblasti, obshtini, kmetstva $row = 2; selishta $row = 3;
 
     $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE); 
-    
+
     $conn2 = new mysqli($servername, $username, $password, $database);
 	if ($conn2->connect_error) {
 		die("Connection failed: " . $conn2->connect_error);
 	} 	  
-	$sql = "SELECT kmetstvo FROM selishta WHERE kmetstvo = '{$rowData[0][0]}'";  // oblasti - oblast, obshtini - obshtina, selishta - kmetstvo: 	  
+	$sql = "SELECT ekatte_num FROM selishta WHERE ekatte_num = '{$rowData[0][0]}'";  // oblasti - oblast, obshtini - obshtina, selishta - ekatte_num, kmetstva - kmetstvo, 	  
 	if ($result = $conn2->query($sql)) {
 		if($result->num_rows <= 0) {
 			$conn2->close(); 
-			$stmt->bind_param($param_types, $rowData[0][0], $rowData[0][1], $rowData[0][2], $rowData[0][3]);
+			$stmt->bind_param($param_types, $rowData[0][0], $rowData[0][1],$rowData[0][2], $rowData[0][3], // need to rebuild these lines based on the prepared statement above
+			                                $rowData[0][4], $rowData[0][5], $rowData[0][7], $rowData[0][8],  
+			                                $rowData[0][9], $rowData[0][10], $rowData[0][11] ); 
 			if(!$stmt->execute()) echo $stmt->error;
 		}
 	} 
